1b49ea1005f64aeae8381b5af15dc7142f02527e
Unknown <imamdwicahyo29@gmail.com>
2019-07-09 07:28:25 +0700

    Add bottom navigation & Fragment Home

--- app/src/main/java/com/idea/idc/myacts/view/HomeActivity.java
+++ app/src/main/java/com/idea/idc/myacts/view/HomeActivity.java
@@ -1,15 +1,67 @@
 package com.idea.idc.myacts.view;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.idea.idc.myacts.R;
+import com.idea.idc.myacts.view.fragment.HomeFragment;
+import com.idea.idc.myacts.view.fragment.ProfilFragment;
+import com.idea.idc.myacts.view.fragment.TaskFragment;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // membuat transparan notifikasi
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            );
+        }
+
+        // set default fragment
+        loadFragment(new HomeFragment());
+
+        // BottomNavigaionView listener
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
+
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.navigation_task:
+                fragment = new TaskFragment();
+                break;
+            case R.id.navigation_profil:
+                fragment = new ProfilFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fl_main, fragment).commit();
+            return true;
+        }
+        return false;
+    }
+
 }
---
+++ app/src/main/java/com/idea/idc/myacts/view/fragment/HomeFragment.java
@@ -0,0 +1,21 @@
+package com.idea.idc.myacts.view.fragment;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.idea.idc.myacts.R;
+
+public class HomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+}
